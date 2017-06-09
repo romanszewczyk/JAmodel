433b9616730a2517fea10b494d3de7a51172ca81
Roman Szewczyk <rszewczyk@onet.pl>
2017-06-09 08:28:21 +0200

    Update JAn_loops.m

--- 10_APPA/anisotropic/JAn_loops.m
+++ 10_APPA/anisotropic/JAn_loops.m
@@ -61,7 +61,9 @@ function BsimT = JAn_loops(HmeasT,a,k,c,Ms,alpha,ModelType,SolverType,IsoAniso,A
 %             0 - isotropic model - DEFAULT
 %             1 - anisotropic model
 %
-% AnisoType - 
+% AnisoType - select the type of anisotropy model
+%               0: uniaxial anisotropy
+%               1: GO anisotropy
 %
 % Kan  - average magnetic anisotropy density, K/m3 (scalar)
 % psi  - the angle between a direction of the easy axis of magnetic anisotropy and the direction of the magnetizing field, rad (scalar)
