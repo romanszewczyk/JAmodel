7053101eef7df2fd79c236e88eb9d3cb445d0c0b
Roman Szewczyk <rszewczyk@onet.pl>
2017-09-29 10:27:31 +0200

    Add files via upload

--- 10_APPA/isotropic/JAsolver.m
+++ 10_APPA/isotropic/JAsolver.m
@@ -50,12 +50,17 @@ function [H,M] = JAsolver(a,k,c,Ms,alpha,Hstart,Hend,M0,ModelType,SolverType,Iso
 % Hend - maximal value of magnetization, A/m (scalar)
 % M0   - initial value of magnetization for ODE, A/m (scalar)
 % SolverType - select the solver for ODE
-%               1 - ode23()
-%               2 - ode45()
-%               3 - ode23s()
-%               4 - rk4() 
+%               0 - ode23()
+%               1 - ode45()
+%               2 - ode23s()
+%               3 - rk4()
+%               other - ode23() 
 % FixedStep - select the solver for integration:
 %               1: quadtrapz(), 0: quadgk()
+%
+% AnisoType - select the type of anisotropy model
+%               0: uniaxial anisotropy
+%               1: GO anisotropy
 %
 % OUTPUT:
 % H - set of output values of magnetizing field H, A/m (vector)
@@ -74,7 +79,7 @@ dMdH_=@(H,M) dMdH(a,k,c,Ms,alpha,M,H,Hstart,Hend,ModelType,IsoAniso,AnisoType,Ka
 
 switch (SolverType)
 
-  case 1
+  case 0
   try
   [H,M] = ode23(dMdH_,[Hstart Hend],M0,options);
   catch
@@ -84,13 +89,28 @@ switch (SolverType)
   end_try_catch
 
 
-  case 2
+  case 1
+  try
   [H,M] = ode45(dMdH_,[Hstart Hend],M0,options);
+  catch
+  H=[Hstart Hend];
+  M=[0 0];
+  fprintf('x');
+  end_try_catch
+  
+  
+  case 2
+  try
+  [H,M] = ode23s(dMdH_,[Hstart Hend],M0,options);
+  catch
+  H=[Hstart Hend];
+  M=[0 0];
+  fprintf('x');
+  end_try_catch
+  
   
   case 3
-  [H,M] = ode23s(dMdH_,[Hstart Hend],M0,options);
-  
-  case 4
+  try
   [H,M] = rk4(dMdH_,[Hstart Hend],M0,50);
 
   otherwise
